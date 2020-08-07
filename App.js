fa037d01b78c050b8da7f699c80a6f79bd5433a1
chintan <chintan.savjiyani@gmail.com>
2020-08-07 21:54:34 +0530

    expo push notification added

--- App.js
+++ App.js
@@ -1,24 +1,57 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AppLoading } from 'expo';
 import { useFonts } from '@use-expo/font';
 import Route from './Route'
 import { delayConfiguration } from 'pusher-redux/react-native'
 import configureStore from './config/store'
+import * as Permissions from 'expo-permissions';
+import * as Notifications from 'expo-notifications';
 import config from './config/app-config'
 import { Provider } from 'react-redux'
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import SafeAreaView from 'react-native-safe-area-view';
 import { View, Text } from 'react-native';
+import { getNotificationPermission } from './src/utils/NotificationPermission';
 const store = configureStore()
 delayConfiguration(store, config.pusherAppId, config.pusherOptions)
 
 
 const App = () => {
+
+  const [expoPushToken, setExpoPushToken] = useState('');
+
   let [fontsLoaded] = useFonts({
     'Poppins-Bold': require('./assets/fonts/Tajawal-Bold.ttf'),
     'Poppins-Medium': require('./assets/fonts/Tajawal-Medium.ttf'),
     'Copperplate': require('./assets/fonts/Copperplate.ttf')
   });
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+  }, []);
+
+  async function registerForPushNotificationsAsync() {
+    let token;
+    const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+    if (existingStatus !== 'granted') {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+    return token;
+  }
+
   // return (
   //   <SafeAreaProvider>
   //     <SafeAreaView>
