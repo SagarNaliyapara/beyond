9338803971b25e68b281810da3c27fab65e37db9
Vishva Shukla <vishva.shukla@noovosoft.com>
2020-08-07 22:39:26 +0530

    add version code for android

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import Route from './Route'
 import { delayConfiguration } from 'pusher-redux/react-native'
 import configureStore from './config/store'
 import * as Permissions from 'expo-permissions';
-import * as Notifications from 'expo-notifications';
+import { Notifications } from 'expo';
 import config from './config/app-config'
 import { Provider } from 'react-redux'
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -30,26 +30,17 @@ const App = () => {
   }, []);
 
   async function registerForPushNotificationsAsync() {
-    let token;
-    const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+    const {status: existingStatus} = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+    let finalStatus = existingStatus;
     if (existingStatus !== 'granted') {
-      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      const {status} = await Permissions.askAsync(Permissions.NOTIFICATIONS);
       finalStatus = status;
     }
     if (finalStatus !== 'granted') {
       alert('Failed to get push token for push notification!');
       return;
     }
-    token = (await Notifications.getExpoPushTokenAsync()).data;
-    if (Platform.OS === 'android') {
-      Notifications.setNotificationChannelAsync('default', {
-        name: 'default',
-        importance: Notifications.AndroidImportance.MAX,
-        vibrationPattern: [0, 250, 250, 250],
-        lightColor: '#FF231F7C',
-      });
-    }
-    return token;
+      return  await Notifications.getExpoPushTokenAsync();
   }
 
   // return (
